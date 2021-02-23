7d5d428ba435daf6eaafa624d7e86729f6b7ebe6
nghiatran <nghiatran.dev@gmail.com>
2021-02-23 12:33:20 +0700

    depth first search

--- search/depth_first_search/main.go
+++ search/depth_first_search/main.go
@@ -1,10 +1,6 @@
-package depth_first_search
+package main
 
-import (
-	"fmt"
-	"github.com/stretchr/testify/assert"
-	"testing"
-)
+import "fmt"
 
 type Node struct {
 	Name     string
@@ -26,24 +22,49 @@ func (n *Node) AddChildren(names ...string) *Node {
 	return n
 }
 
-func TestDepthFirstSearch(t *testing.T) {
+func main() {
 	var graph = NewNode("A").AddChildren("B", "C", "D")
 	graph.Children[0].AddChildren("E").AddChildren("F")
 	graph.Children[2].AddChildren("G").AddChildren("H")
 	graph.Children[0].Children[1].AddChildren("I").AddChildren("J")
 	graph.Children[2].Children[0].AddChildren("K")
 	output := graph.DepthFirstSearch([]string{})
-	expected := []string{"A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"}
+	expectation := []string{"A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"}
 
-	assert.Equal(t, output, expected, "depth first search wrong!!!")
+	fmt.Println(output)
+	fmt.Println(expectation)
+
+	//fmt.Println(test([]int{0}))
 }
 
-//========================================================================
+//func test(arr []int) []int {
+//	count := arr[len(arr)-1]
+//	arr = append(arr, count+1)
+//	if count < 10 {
+//		return test(arr)
+//	}
+//	return arr
+//}
 
 func (n *Node) DepthFirstSearch(array []string) []string {
 	// Write your code here.
 
-	fmt.Println(array)
+	//fmt.Println(n)
+	output := []string{"A"}
+	output = n.Search(output)
 
-	return nil
+	return output
+}
+
+func (n *Node) Search(result []string) []string {
+
+	for _, child := range n.Children {
+		result = append(result, child.Name)
+		//check child node, if != nil, call de quy
+		if child.Children != nil {
+			result = child.Search(result)
+		}
+	}
+
+	return result
 }
